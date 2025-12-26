b2f43ceea8bb287d529a94dee9e828ad9dcab131
Ritik <ritikyadav2426@gmail.com>
2025-12-26 22:43:15 +0530

    implemented cd command logic

--- shell/executor.py
+++ shell/executor.py
@@ -1,17 +1,27 @@
 import subprocess
 import shlex
+import os
+
+class ShellState:
+                                    #this part just holds the state or  contains current working direcotry(cwd)
+    def __init__(self):
+        self.cwd = os.getcwd()      #just abject which holds the current working direcotry
+
+state = ShellState()
+
 
 def execute_command(command : str):
     try:
         args = shlex.split(command)
+        cmd = args[0]
 
-        #temporary placeholder for custom commands 
-        if args[0] in {"ss" , "github" , "leetcode"}:
-            print(f"[custom command detected] -> {args[0]}")
-            return
+        if cmd == 'cd':
+            change_directory(args)
+            return 
 
         subprocess.run(
             args,
+            cwd=state.cwd,          #whenever subprocess runs a command , run in this cwd , this is to ensure that after cd in the future , the command runs in that direcotry not this one
             shell=False
         )
 
@@ -19,3 +29,18 @@ def execute_command(command : str):
         print(f"command not found: {args[0]}")
     except Exception as e:
         print(f"error: {e}")
+
+
+def change_directory(args):
+    if len(args) == 1:
+        target = os.path.expanduser("~")
+    else:
+        target = args[1]
+
+    new_path = os.path.abspath(os.path.join(state.cwd, target))
+
+    if not os.path.isdir(new_path):
+        print(f"cd: no such directory: {target}")
+        return
+
+    state.cwd = new_path
